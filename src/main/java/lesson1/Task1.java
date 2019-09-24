726ae3b69bfe8094fb081a0a16e80803808c51da
vlad <tkach771@gmail.com>
2019-09-24 10:27:12 +0300

    переделал 1 задание с изминениями

--- src/main/java/lesson1/Task1.java
+++ src/main/java/lesson1/Task1.java
@@ -1,20 +1,21 @@
-package lesson1;
+package lesson1
 
-public class Task1 {
-    {
-        public static void main (String[]args){
-        System.out.println( min (5, 2, 3));
+public class task1 {
+
+    public static void main(String[] args) {
+        System.out.println(min(5, 2, 3));
     }
 
-        public static int min ( int a, int b, int c){
+    public static int min(int a, int b, int c) {
         return min(min(a, b), c);
     }
 
-        public static int min ( int a, int b){
+    public static int min(int a, int b) {
         if (a > b) {
             return b;
         }
         return a;
     }
-    }
 }
+
+
